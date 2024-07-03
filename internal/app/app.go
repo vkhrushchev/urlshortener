fe93d5f745d4911bc1c122d7147eb685eda864c1
Хрущев Вячеслав <khrushchev.v@otr.ru>
2024-07-03 15:13:12 +0700

    iter4-2 minor changes

--- internal/app/app.go
+++ internal/app/app.go
@@ -44,41 +44,37 @@ func (a *URLShortenerApp) Run() error {
 }
 
 func (a *URLShortenerApp) createShortURLHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		rawBody := make([]byte, r.ContentLength)
-		_, err := r.Body.Read(rawBody)
-		if err != nil && err != io.EOF {
-			_ = fmt.Errorf("app: error reading requestBody: %v", err)
+	rawBody := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(rawBody)
+	if err != nil && err != io.EOF {
+		_ = fmt.Errorf("app: error reading requestBody: %v", err)
 
-			w.WriteHeader(http.StatusInternalServerError)
-			_, _ = w.Write([]byte("app: error reading requestBody"))
-
-			return
-		}
-
-		urlID := util.RandStringRunes(10)
-		for a.urls[urlID] != "" {
-			urlID = util.RandStringRunes(10)
-		}
-
-		a.urls[urlID] = string(rawBody)
-
-		w.Header().Add("Content-Type", "plain/text")
-		w.WriteHeader(http.StatusCreated)
-
-		var shortUrl string
-		if strings.HasSuffix(a.baseURL, "/") {
-			shortUrl = a.baseURL + urlID
-		} else {
-			shortUrl = a.baseURL + "/" + urlID
-		}
-
-		_, _ = w.Write([]byte(shortUrl))
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte("app: error reading requestBody"))
 
 		return
 	}
 
-	w.WriteHeader(http.StatusBadRequest)
+	urlID := util.RandStringRunes(10)
+	for a.urls[urlID] != "" {
+		urlID = util.RandStringRunes(10)
+	}
+
+	a.urls[urlID] = string(rawBody)
+
+	w.Header().Add("Content-Type", "plain/text")
+	w.WriteHeader(http.StatusCreated)
+
+	var shortUrl string
+	if strings.HasSuffix(a.baseURL, "/") {
+		shortUrl = a.baseURL + urlID
+	} else {
+		shortUrl = a.baseURL + "/" + urlID
+	}
+
+	_, _ = w.Write([]byte(shortUrl))
+
+	return
 }
 
 func (a *URLShortenerApp) getURLHandler(w http.ResponseWriter, r *http.Request) {
